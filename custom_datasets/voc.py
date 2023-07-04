e9bd11c2939bf5b0516bdd3537399598f1d395bd
NicolaDean <nicola.dean@mail.polimi.it>
2023-07-04 19:21:33 +0200

    .

--- custom_datasets/voc.py
+++ custom_datasets/voc.py
@@ -4,6 +4,7 @@ from lxml import etree
 import os
 import tqdm
 from PIL import Image
+from augmentation import apply as apply_aug
 
 image_size = (300, 300)
 
@@ -111,7 +112,7 @@ class Voc:
         #Extract Bounding boxes
         bbox    = []
         labels  = []
-
+        
         path = os.path.join(path, sample_name + '.xml')
         with tf.compat.v1.gfile.GFile(path, 'r') as fid:
             xml_str = fid.read()
@@ -125,22 +126,37 @@ class Voc:
                 ymin = (float(obj['bndbox']['ymin']) / ih)
                 xmax = (float(obj['bndbox']['xmax']) / iw)
                 ymax = (float(obj['bndbox']['ymax']) / ih)
-
+                
+                
                 bbox.append([xmin,ymin,xmax,ymax])
                 #Labels INFORMATION
                 label = obj['name']
                 label = labels_dict[label]
-                labels.append(label)
+                labels.append(tf.cast(label, tf.int32))
 
-                #TODO LABEL MAT DICT
-                #classes.append(label_map_dict[obj['name']])
-        
-        for _ in range(len(bbox),20):
-           bbox.append([0,0,0,0])
-           labels.append(-1)
-        
-        return img, tf.stack(bbox), tf.stack(labels)
+        bbox = tf.clip_by_value(tf.stack(bbox), clip_value_min=0, clip_value_max=1)
 
+        img, bbox = apply_aug(tf.constant(img,dtype=tf.float32),bbox)
+        return img, bbox, labels
+
+    def pad_custom_data(batch_size,boxes,labels):
+        max_len = 0
+
+        for idx in range(batch_size):
+            if len(boxes[idx]) > max_len : max_len = len(boxes[idx])
+        
+        for idx in range(batch_size):
+            for _ in range(len(boxes[idx]),max_len):
+                labels[idx] = tf.concat([labels[idx],[-1]],0)
+                boxes[idx]  = tf.concat([boxes[idx],[[0,0,0,0]]],0)
+            boxes[idx]  = tf.stack(boxes[idx])
+            labels[idx] = tf.stack(labels[idx])
+        
+        boxes  = tf.stack(boxes)
+        labels = tf.stack(labels)
+
+        return boxes,labels
+        
     def get_custom_data_generator(path,batch_size=32):
         #Load samples names
         sample_names = Voc.get_list_of_sample_names(path)
@@ -181,7 +197,10 @@ class Voc:
                 #Shuffle Data On Epoch End
                 if idx == 0:
                     random.shuffle(sample_names)
-            yield tf.stack(samples_dataset),tf.stack(boxes_dataset),tf.stack(labels_dataset)
+            
+            boxes_dataset,labels_dataset = Voc.pad_custom_data(batch_size,boxes_dataset,labels_dataset)
+
+            yield tf.stack(samples_dataset),boxes_dataset, labels_dataset
             
             
 
--- simulate_run.py
+++ simulate_run.py
@@ -1,15 +1,50 @@
 from evaluator import Evaluator
 from trainer import Trainer
 
+
 batch_size = 32
 epochs = 10
 
 from custom_datasets import Voc
 
+
+
 trainer = Trainer(epochs, batch_size)
 trainer.initialize_experiment()
-#trainer.fit("output/temp.h5")
+trainer.fit("output/temp.h5")
 
+'''
 evaluator = Evaluator(batch_size)
 evaluator.initialize()
 evaluator.evaluate("output/temp.h5")
+'''
+
+from models.decoder import get_decoder_model
+from utils import bbox_utils, data_utils, train_utils, eval_utils,drawing_utils
+from models.ssd_mobilenet_v2 import get_model
+from custom_datasets.voc import labels
+
+test, test_size = Voc.get_custom_data_generator('./train')
+imgs, gt_boxes, gt_labels = next(test)
+
+hyper_params = train_utils.get_hyper_params()
+
+_labels = ['bg'] + labels
+hyper_params["total_labels"] = len(_labels)
+
+# We calculate prior boxes for one time and use it for all operations because of the all images are the same sizes
+prior_boxes    = bbox_utils.generate_prior_boxes(hyper_params["feature_map_shapes"], hyper_params["aspect_ratios"])
+
+ssd_model = get_model(hyper_params)
+
+ssd_model.load_weights("output/temp.h5")
+
+ssd_decoder_model = get_decoder_model(ssd_model, prior_boxes, hyper_params)
+
+print(f'img shape: {imgs[0].shape}')
+
+pred_bboxes, pred_labels, pred_scores = ssd_decoder_model.predict(imgs,verbose=1)
+
+print(pred_bboxes)
+drawing_utils.draw_img_prediction(imgs[0],pred_bboxes[0],pred_labels[0],pred_scores[0],_labels,batch_size)
+
